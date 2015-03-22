e3cf3045c909326cf2234ed9d08753d1f8c08bc2
cshain <christophershain@gmail.com>
2015-03-21 21:28:54 -0400

    Added VSPackage that creates a single file generator for Cap'n Proto schema files (.capnp) and uses CapnProto.net to compile them on the fly into C# flyweights.

---
+++ CapnProto.net-VSPackage/CapnProtoSingleFileGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using CapnProtonet.CapnProto_VSPackage;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace CapnProtonet.CapnProto_net_VSPackage
+{
+    // Note: the class name is used as the name of the Custom Tool from the end-user's perspective.
+    [ComVisible(true)]
+    [Guid("18786B62-E81D-45CD-9E91-DCCF75E5E4FB")]
+    [ProvideObject(typeof(CapnProtoSingleFileGenerator))]
+    [ProvideCodeGeneratorExtension("CapnProtoSingleFileGenerator", ".capnp")]
+    [CodeGeneratorRegistration(typeof(CapnProtoSingleFileGenerator), "CapnProtoSingleFileGenerator", VsContextGuids.VsContextGuidVcsProject, GeneratesDesignTimeSource = true)]
+    public class CapnProtoSingleFileGenerator : CustomToolBase
+    {
+        protected override string DefaultExtension()
+        {
+            return ".cs";
+        }
+
+        protected override byte[] Generate(string inputFilePath, string inputFileContents, string defaultNamespace, IVsGeneratorProgress progressCallback)
+        {
+            var localDir = Path.GetDirectoryName(typeof (CapnProtoSingleFileGenerator).Assembly.Location);
+
+            var capnpProcess = new Process
+                {
+                    StartInfo =
+                        {
+                            FileName = Path.Combine(localDir, "capnp.exe"),
+                            Arguments = String.Format("compile -ocsharp \"{0}\"", inputFilePath),
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                            WorkingDirectory = localDir,
+                            RedirectStandardInput = true,
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                        }, 
+                };
+
+            capnpProcess.Start();
+            capnpProcess.StandardInput.Write(inputFileContents);
+
+            var stdOut = capnpProcess.StandardOutput.ReadToEnd();
+            var stdErr = capnpProcess.StandardError.ReadToEnd();
+            if (!String.IsNullOrEmpty(stdErr))
+            {
+                return new UTF8Encoding(true).GetBytes(stdErr); 
+            }
+
+            capnpProcess.Close();
+
+            return new UTF8Encoding(true).GetBytes(stdOut);
+        }
+    }
+}
---
+++ CapnProto.net-VSPackage/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace CapnProtonet.CapnProto_net_VSPackage
+{
+    static class GuidList
+    {
+        public const string guidCapnProto_net_VSPackagePkgString = "efb9fe69-5be7-42bd-8677-9d420798dc8b";
+        public const string guidCapnProto_net_VSPackageCmdSetString = "62e67643-dbf0-4769-84d8-9433c0f9e869";
+
+        public static readonly Guid guidCapnProto_net_VSPackageCmdSet = new Guid(guidCapnProto_net_VSPackageCmdSetString);
+    };
+}
